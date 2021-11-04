9263b063cfab2afbd51d53970992badc8fa5c7b1
zyavuz610 <43997998+zyavuz610@users.noreply.github.com>
2021-11-04 09:25:18 +0300

    Create jscript.js

---
+++ src/jscript.js
@@ -0,0 +1,19 @@
+function sortArray(arr) {
+  var len = arr.length;
+  for (var i = 0; i < len; i++) {
+    for (var j = 0; j < len - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        var temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  return arr;
+}
+
+function swap(a, b) {
+  var temp = a;
+  a = b;
+  b = temp;
+}
