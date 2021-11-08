b77a0d0611f85debfc034ce02077992091356cc5
zyavuz610 <43997998+zyavuz610@users.noreply.github.com>
2021-11-08 19:59:05 +0300

    ders sonu kodlar

---
+++ src/07/simple-form.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+
+<form action="welcome.php" method="post">
+    Ad: <input type="text" name="name"><br>
+    E-Posta: <input type="text" name="email"><br>
+    <input type="submit" value="Gönder">
+</form>
+
+</body>
+</html>
---
+++ src/07/welcome.php
@@ -0,0 +1,8 @@
+<html>
+<body>
+
+Merhaba <?php echo $_POST["name"]; ?><br>
+Girdiğiniz eposta adresi: <?php echo $_POST["email"]; ?>
+
+</body>
+</html>
