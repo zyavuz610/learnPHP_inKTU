b78e25266daf99435176f1ec31a0e2257c8e1c8e
zyavuz610 <43997998+zyavuz610@users.noreply.github.com>
2021-11-08 20:29:09 +0300

    array codes added

---
+++ src/2021-fall(2021-Güz)/06-arrays/array_with_loop.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$cars = array("Volvo", "BMW", "Toyota");
+$arrlength = count($cars);
+
+for($x = 0; $x < $arrlength; $x++) {
+  echo $cars[$x];
+  echo "<br>";
+}
+?>
+
+</body>
+</html>
---
+++ src/2021-fall(2021-Güz)/06-arrays/simple-array.php
@@ -0,0 +1,6 @@
+<?php
+$cars = array("Volvo", "BMW", "Toyota");
+echo "I like " . $cars[0] . ", " . $cars[1] . " and " . $cars[2] . ".";
+
+echo count($cars);
+?>
